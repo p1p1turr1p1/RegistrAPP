a0f75eac16cabff93b81488dda9aed4941a70ef2
vic-valenzuelaa <vic.valenzuelaa@duocuc.cl>
2024-01-05 10:18:05 -0300

    Animacion

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AnimationController, IonCard, IonTitle } from '@ionic/angular';
+import { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChildren(IonTitle, {read: ElementRef})
+  titleElements!:QueryList<ElementRef<HTMLIonTitleElement>>;
 
+  private animation!:Animation; 
+  constructor(private animationCtrl: AnimationController) { }
   ngOnInit(){
 
   }
-  ingresar(){
-    console.log("HICE CLICK");
+
+  ngAfterViewInit(){
+    const testUser = this.animationCtrl
+    .create()
+    .addElement(this.titleElements.get(0)!.nativeElement)
+    .fromTo('opacity', '0', '1');
+
     
+
+    this.animation=this.animationCtrl
+    .create()
+    .duration(2000)
+    .iterations(1)
+    .addAnimation([testUser]);
+
+    this.animation.play();
   }
 }
