55afc9652e21c39202060b8cc27cf61ecb923511
vic-valenzuelaa <vic.valenzuelaa@duocuc.cl>
2024-01-07 22:18:04 -0300

    animacion login logo

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AnimationController, IonImg, IonTitle } from '@ionic/angular';
+import { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,40 @@ user={
   usuario:"",
   clave:""
 }
-  constructor() { }
-
   ngOnInit() {
   }
+  @ViewChildren(IonTitle, {read: ElementRef})
+  titleElements!:QueryList<ElementRef<HTMLIonTitleElement>>;
+  @ViewChildren(IonImg, {read:ElementRef})
+  iconElements!:QueryList<ElementRef<HTMLIonImgElement>>;
 
+  private animation!:Animation; 
+  constructor(private animationCtrl: AnimationController) { }
+
+  ngAfterViewInit(){
+    console.log(this.titleElements);
+    const testUser = this.animationCtrl
+    .create()
+    .addElement(this.titleElements.get(0)!.nativeElement)
+    .fromTo('opacity', '0', '1');
+
+    const logo = this.animationCtrl
+    .create()
+    .addElement(this.iconElements.get(0)!.nativeElement)
+
+    .keyframes([
+      { offset: 0, transform: 'scale(1) rotate(0)' },
+      { offset: 0.25, transform: 'scale(1) rotate(45deg)' },
+      { offset: 0.75, transform: 'scale(1) rotate(-45deg)' },
+      { offset: 1, transform: 'scale(1) rotate(0)' }
+    ]);
+
+    this.animation=this.animationCtrl
+    .create()
+    .duration(1000)
+    .iterations(1)
+    .addAnimation([testUser,logo]);
+
+    this.animation.play();
+  }
 }
